57b84a4b33e14d7b24b8384b7013bcf2615f8568
Timur Kazbekov <timur.kazbekov@milrem.com>
2022-08-01 18:42:26 +0300

    add creating of a new board when no id is provided in the url

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
-    <BoardColumnItem v-for="(column, index) in boardStore.board?.columns" :key="index" :column="column"></BoardColumnItem>
+    <BoardColumnItem v-for="(column, index) in boardStore.board?.columns" :key="index" :column="column">
+    </BoardColumnItem>
   </main>
 </template>
 
@@ -8,17 +9,16 @@
 import BoardColumnItem from "../components/BoardColumnItem.vue";
 import { useBoardStore } from "@/stores/board";
 import type { Socket } from "socket.io-client";
-import { inject } from "vue";
-import { useRoute } from "vue-router";
+import { inject, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import type { Note } from "@/models/Note";
 
 const route = useRoute();
+const router = useRouter();
 
 const socket = inject('socket') as Socket;
 socket.on("connect", () => {
-  if (route.params.id) {
-    socket.emit("get_board", route.params.id)
-  }
+  route.params.id ? socket.emit("get_board", route.params.id) : socket.emit("create_board")
 })
 const boardStore = useBoardStore();
 
@@ -32,6 +32,18 @@ socket.on("note_update", (note: Note, columnId: string) => {
   console.log(note, columnId);
   boardStore.addNoteAndSort(note, columnId)
 })
+
+socket.on("board_created", (id: string) => {
+  router.push({ name: "board", params: { id: id } });
+})
+
+watch(
+  () => route.params.id,
+  async newId => {
+    socket.emit("get_board", newId)
+  }
+)
+
 
 </script>
 
